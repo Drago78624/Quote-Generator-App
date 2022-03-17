91b57f530339a75c977ced2587f369bf896d579d
Muhammad Maaz Ahmed <60292456+Drago78624@users.noreply.github.com>
2022-03-17 16:03:25 +0500

    dfdg

--- script.js
+++ script.js
@@ -2,16 +2,29 @@ const btn = document.querySelector(".btn");
 const animeName = document.querySelector(".name");
 const character = document.querySelector(".character");
 const quoteC = document.querySelector(".quote");
+const body = document.querySelector("body");
 
 const getQuote = function () {
   fetch("https://animechan.vercel.app/api/random")
     .then((response) => response.json())
     .then((quote) => {
+      const name = String(quote.anime).toLowerCase();
+      getWp(name);
       animeName.textContent = quote.anime;
       character.textContent = quote.character;
       quoteC.textContent = quote.quote;
-      console.log(quote);
     });
 };
+
+function getWp(animeName) {
+  fetch(
+    `https://wallpaper-api-234.herokuapp.com/wallpapers?name=${animeName}&api_key=maazilovesmiki`
+  )
+    .then((response) => response.json())
+    .then((wp) => {
+      const [wallpaper] = wp.wallpapers;
+      body.style.backgroundImage = `url(${wallpaper})`;
+    });
+}
 
 btn.addEventListener("click", getQuote);
